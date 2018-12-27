09c40c149eb9f86bd6df538c8740d2251cbfec2c
Andrew Mason <andrew.mason@n2pub.com>
2018-12-27 11:33:29 -0500

    add executable

--- lib/contributor_covenant/install.rb
+++ lib/contributor_covenant/install.rb
@@ -2,4 +2,4 @@
 
 require "rake"
 
-load File.expand_path(File.join(File.dirname(__FILE__), "tasks/install.rake"))
+load File.expand_path(File.join(File.dirname(__FILE__), "tasks/generate_contributor_covenant.rake"))
